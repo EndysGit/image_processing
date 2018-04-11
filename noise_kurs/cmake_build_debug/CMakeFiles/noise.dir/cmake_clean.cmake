428094ecd2559a09b212db7652f6a1722c25c9e9
Vorfolomeev Gleb <vorfolomeev@Vorfolomeevs-MacBook-Pro.local>
2018-04-11 18:32:08 +0300

    fix

---
+++ noise_kurs/cmake_build_debug/CMakeFiles/noise.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/noise.dir/main.cpp.o"
+  "CMakeFiles/noise.dir/FastNoise.cpp.o"
+  "noise.pdb"
+  "noise"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/noise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
